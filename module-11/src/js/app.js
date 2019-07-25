8bfd6c49ad565ba4e7c0457da62277c252c83a5c
kostyash23 <kostiashmotoloha@gmail.com>
2019-07-25 15:56:11 +0300

    module-11

--- module-11/src/js/app.js
+++ module-11/src/js/app.js
@@ -1,30 +1,39 @@
+import MicroModal from 'micromodal';
+import { Notyf } from 'notyf';
 import { PRIORITY_TYPES, ICON_TYPES, NOTE_ACTIONS, } from './utils/constants';
 import initialNotes from '../assets/notes.json';
 import Notepad from './utils/notepad-model';
 import { getRefs, renderNoteList, createListItem } from './utils/view.js';
+import noteTemplate from '../templates/note.hbs';
 
 
+MicroModal.init();
+const notyf = new Notyf();
 const shortid = require('shortid');
+const refs = getRefs();
 const notepad = new Notepad(initialNotes);
 
-const refs = getRefs();
 
 renderNoteList(refs.list, notepad.notes);
+const addListItem = (listRef, note) => {
+    listRef.insertAdjacentHTML('beforeend', noteTemplate(note));
 
-
+}
 const removeListItem = (item) => {
     notepad.deleteNote(item.dataset.id);
     item.remove();
-}
-const addListItem = (listRef, note) => {
-    listRef.insertAdjacentHTML('beforeend',noteTemplate(note));
 }
 
 
 
 const handleEditorSubmit = event => {
     event.preventDefault()
-    if (!refs.titleField.value || !refs.bodyField.value) return alert('Необходимо заполнить все поля!');
+    if (!refs.titleField.value || !refs.bodyField.value) return notyf.error('Необходимо заполнить все поля!');
+    if (refs.titleField.value || refs.bodyField.value) {
+        
+        MicroModal.close('note-editor-modal')
+        notyf.success('Заметка добавлена успешно!');
+    }
     const newNote = {
         id: shortid.generate(),
         title: refs.titleField.value,
@@ -33,13 +42,22 @@ const handleEditorSubmit = event => {
     }
     notepad.saveNote(newNote);
     addListItem(refs.list, newNote)
+
+
 }
 const handleDelete = event => {
     if (event.target.parentNode.dataset.action === 'delete-note')
         removeListItem(event.target.closest('li'));
+    notyf.success('Заметка удалена')
 }
 
-refs.editor.addEventListener('submit', handleEditorSubmit)
+
+
+refs.btnOpen.addEventListener('click', () => {
+    MicroModal.show('note-editor-modal');
+});
+
+refs.form.addEventListener('submit', handleEditorSubmit)
 refs.list.addEventListener('click', handleDelete)
 refs.search.addEventListener('input', (e) => {
     renderNoteList(refs.list, notepad.filterNotesByQuery(e.target.value))
